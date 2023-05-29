1e72ed39dc77f31a92ff53e614a094838070195e
Otto <justigo86@gmail.com>
2023-05-28 20:44:29 -0400

    updated Register and SignIn to localhost - changing from Heroku/main branch.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
-import React, {useState}from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import SignIn from './components/SignIn/SignIn';
-import Register from './components/Register/Register';
-import Logo from './components/Logo/Logo';
-import Rank from './components/Rank/Rank';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Particles from 'react-particles-js';
-import styled from 'styled-components';
-// import Clarifai from 'clarifai';
+import React, { useState } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Particles from "react-particles-js";
+import styled from "styled-components";
+import Clarifai from "clarifai";
 
-const AppDiv = styled.div `
-  text-align:center;
+// const clarifaiRequest = (imgURL) => {
+//   // Your PAT (Personal Access Token) can be found in the portal under Authentification
+//   const PAT = "dfc62ad7090f4c5db9a89d0ff756090a";
+//   // Specify the correct user_id/app_id pairings
+//   // Since you're making inferences outside your app's scope
+//   const USER_ID = "justigo86";
+//   const APP_ID = "Face_Detection_API";
+//   // Change these to whatever model and image URL you want to use
+//   // const MODEL_ID = 'face-detection';
+//   const IMAGE_URL = imgURL;
+
+//   const raw = JSON.stringify({
+//     user_app_id: {
+//       user_id: USER_ID,
+//       app_id: APP_ID,
+//     },
+//     inputs: [
+//       {
+//         data: {
+//           image: {
+//             url: IMAGE_URL,
+//           },
+//         },
+//       },
+//     ],
+//   });
+
+//   return {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       Authorization: "Key " + PAT,
+//     },
+//     body: raw,
+//   };
+// };
+
+// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+// this will default to the latest version_id
+
+const AppDiv = styled.div`
+  text-align: center;
   & > .particles {
     position: fixed;
     top: 0;
@@ -21,40 +62,40 @@ const AppDiv = styled.div `
     left: 0;
     z-index: -1;
   }
-`
+`;
 
-// const app = new Clarifai.App({
-//   apiKey: ''
-//  });    //API key is vulnerability and needs to be moved to back-end for security
+const app = new Clarifai.App({
+  apiKey: "4460d3f4e3da44c78375c06c03ed2d25",
+}); //API key is vulnerability and needs to be moved to back-end for security
 
 function App() {
-  const [input, setInput] = useState('');
-  const [imgURL, setImgURL] = useState('');
+  const [input, setInput] = useState("");
+  const [imgURL, setImgURL] = useState("");
   const [box, setBox] = useState({});
-  const [route, setRoute] = useState('signin');
+  const [route, setRoute] = useState("signin");
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [user, setUser] = useState({
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
+    joined: "",
   });
 
   const initialState = () => {
-    setInput('');
-    setImgURL('');
+    setInput("");
+    setImgURL("");
     setBox({});
-    setRoute('signin');
+    setRoute("signin");
     setIsSignedIn(false);
     setUser({
-      id: '',
-      name: '',
-      email: '',
+      id: "",
+      name: "",
+      email: "",
       entries: 0,
-      joined: ''
+      joined: "",
     });
-  }
+  };
 
   /*
   useEffect(() => {               //useEffect used to fetch server API
@@ -70,30 +111,30 @@ function App() {
       name: data.name,
       email: data.email,
       entries: data.entries,
-      joined: data.joined
-    })
-  }
+      joined: data.joined,
+    });
+  };
 
   const calculateFaceLocation = (data) => {
     const face = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const image = document.getElementById("inputimage");
     const width = image.width;
     const height = image.height;
     return {
       leftCol: face.left_col * width,
       topRow: face.top_row * height,
-      rightCol: width - (face.right_col * width),
-      bottomRow: height - (face.bottom_row * height)
-    }
-  }
+      rightCol: width - face.right_col * width,
+      bottomRow: height - face.bottom_row * height,
+    };
+  };
 
   const displayFaceBox = (box) => {
     setBox(box);
-  }
+  };
 
   const onInputChange = (e) => {
     setInput(e.target.value);
-  }
+  };
 
   // Clarifai.COLOR_MODEL
   // Clarifai.FACE_DETECT_MODEL
@@ -101,74 +142,93 @@ function App() {
   // "a403429f2ddf4b49b307e318f00e528b"
 
   const onButtonSubmit = () => {
-    setImgURL(input);      //set image to variable
-    //fetch added after moving API key to backend (image.js)
-    fetch('https://floating-waters-88143.herokuapp.com/imageurl', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        input: input
-      })
-    })
-      .then(response => response.json())  //fetched data needs to be converted into JSON
-      .then(response => {     //clarifai provides a response
-        if (response) {       //if the response is received
-          fetch('https://floating-waters-88143.herokuapp.com/image', {    //fetch route of image
-            method: 'put',                     //ensuring method is PUT
-            headers: {'Content-Type': 'application/json'},  //clarifying header info
+    setImgURL(input); //set image to variable
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, input) //look for face within image w.API
+      //fetch using
+      // fetch(
+      //   "https://api.clarifai.com/v2/models/face-detection/outputs",
+      //   clarifaiRequest(input)
+      // )
+      // .then((response) => response.json())
+      //fetch added after moving API key to backend (image.js)
+      // fetch('https://floating-waters-88143.herokuapp.com/imageurl', {
+      //   method: 'post',
+      //   headers: {'Content-Type': 'application/json'},
+      //   body: JSON.stringify({
+      //     input: input
+      //   })
+      // })
+      // .then((response) => response.json()) //fetched data needs to be converted into JSON
+      .then((response) => {
+        //clarifai provides a response
+        if (response) {
+          //if the response is received
+          fetch("http://localhost:3000/image", {
+            // fetch('https://floating-waters-88143.herokuapp.com/image', {    //fetch route of image
+            method: "put", //ensuring method is PUT
+            headers: { "Content-Type": "application/json" }, //clarifying header info
             body: JSON.stringify({
-                id: user.id,
-            })   
+              id: user.id,
+            }),
           })
-          .then(response => response.json())
-          .then(count => {
-            setUser(prevState => ({           //code in course did not work with hooks
-                ...prevState, entries: count  //using ...prevState w.spread updates entries immediately
-            }))                               //by automatically merging update objects
-          })
-          .catch(err => console.log("error", err))
+            .then((response) => response.json())
+            .then((count) => {
+              setUser((prevState) => ({
+                //code in course did not work with hooks
+                ...prevState,
+                entries: count, //using ...prevState w.spread updates entries immediately
+              })); //by automatically merging update objects
+            })
+            .catch((err) => console.log("error", err));
         }
-        displayFaceBox(calculateFaceLocation(response))})
-    .catch(err => console.log("error", err));
-  }
+        displayFaceBox(calculateFaceLocation(response));
+      })
+      .catch((err) => console.log("error", err));
+  };
 
   const onRouteChange = (route) => {
-    if (route === 'signout') {
+    if (route === "signout") {
+      // setIsSignedIn(false);
       initialState();
-    } else if (route === 'home') {
+    } else if (route === "home") {
       setIsSignedIn(true);
     }
     setRoute(route);
-  }
+  };
 
   return (
     <AppDiv className="App">
-      <Particles className="particles"
+      <Particles
+        className="particles"
         params={{
           particles: {
             number: {
               value: 30,
               density: {
                 enable: true,
-                value_area: 300
-              }
-            }
-          }
+                value_area: 300,
+              },
+            },
+          },
         }}
       />
       <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
-      { route === 'home' ? 
-        <div id='wrap-in-div-to-avoid-error'>
+      {route === "home" ? (
+        <div id="wrap-in-div-to-avoid-error">
           <Logo />
           <Rank name={user.name} entries={user.entries} />
-          <ImageLinkForm  onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
           <FaceRecognition box={box} imgURL={imgURL} />
-        </div> :
-        (route === 'signin' ?
-        <SignIn loadUser={loadUser} onRouteChange={onRouteChange} /> :
+        </div>
+      ) : route === "signin" ? (
+        <SignIn loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
         <Register loadUser={loadUser} onRouteChange={onRouteChange} />
-        )
-      }
+      )}
     </AppDiv>
   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,96 +1,95 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Register = ({ onRouteChange, loadUser }) => {
-    const [name, setName] = useState('default')
-    const [email, setEmail] = useState('default2')
-    const [password, setPassword] = useState('default3')
-    const onNameChange = (e) => {
-        setName(e.target.value);
-    }
-    const onEmailChange = (e) => {
-        setEmail(e.target.value);
-    }
-    const onPasswordChange = (e) => {
-        setPassword(e.target.value);
-    }
-    const onSubmitRegistration = () => {              //when clicking submit
-        fetch('https://floating-waters-88143.herokuapp.com/register', { //fetching server API from localhost
-            method: 'post',                     //ensuring method is POST
-            headers: {'Content-Type': 'application/json'},  //clarifying header info
-            body: JSON.stringify({
-                name: name,
-                email: email,
-                password: password
-            })             //stringify the json data
-        })
-        .then(res => res.json())
-        .then(user => {
-            if (user.id) {          //user must have an id - else will be able to sign in without validation
-                loadUser(user)
-                onRouteChange('home');
-            }
-        })
-    }
-    return (
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80">
-                <div className="measure center">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f2 fw6 ph0 mh0">Register</legend>
-                        <div className="mt3">
-                            <label 
-                                className="db fw6 lh-copy f6" 
-                                htmlfor="email-address"
-                            >
-                                    Name
-                            </label>
-                            <input
-                                onChange={onNameChange} 
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                type="text" 
-                                name="name"  
-                                id="name" />
-                        </div>
-                        <div className="mt3">
-                            <label 
-                                className="db fw6 lh-copy f6" 
-                                htmlfor="email-address"
-                            >
-                                Email
-                            </label>
-                            <input 
-                                onChange={onEmailChange}
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                type="email" 
-                                name="email-address"  
-                                id="email-address" />
-                        </div>
-                        <div className="mv3">
-                            <label 
-                                className="db fw6 lh-copy f6" 
-                                htmlfor="password"
-                            >
-                                Password
-                            </label>
-                            <input 
-                                onChange={onPasswordChange}
-                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                                type="password" 
-                                name="password"  
-                                id="password" />
-                        </div>
-                    </fieldset>
-                    <div className="">
-                        <input 
-                            onClick={onSubmitRegistration} 
-                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                            type="submit" 
-                            value="Register" />
-                    </div>
-                </div>
-            </main>
-        </article>
-    )
-}
+  const [name, setName] = useState("default");
+  const [email, setEmail] = useState("default2");
+  const [password, setPassword] = useState("default3");
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const onSubmitRegistration = () => {
+    fetch("http://localHost:3000/register", {
+      //if using Heroku
+      // fetch("https://floating-waters-88143.herokuapp.com/register", {
+      //fetching server API from localhost
+      method: "post", //ensuring method is POST
+      headers: { "Content-Type": "application/json" }, //clarifying header info
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }), //stringify the json data
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        if (user.id) {
+          //user must have an id - else will be able to sign in without validation
+          loadUser(user);
+          onRouteChange("home");
+        }
+      });
+  };
+  return (
+    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <div className="measure center">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f2 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Name
+              </label>
+              <input
+                onChange={onNameChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                onChange={onEmailChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                onChange={onPasswordChange}
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={onSubmitRegistration}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Register"
+            />
+          </div>
+        </div>
+      </main>
+    </article>
+  );
+};
 
-export default Register
+export default Register;
